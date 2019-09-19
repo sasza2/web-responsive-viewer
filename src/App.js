2fa8ea79cd26f80cfc8061bd348329be8e7f79e6
sasza <sasza@onet.eu>
2019-09-19 21:10:24 +0200

    webview component 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
-const App = () => <span>elo</span>
+const App = () => (    
+  <div dangerouslySetInnerHTML={{ __html: '<webview src="https://www.github.com/" disablewebsecurity></webview>'}} /> 
+)
 
 export default App
