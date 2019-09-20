e22a2d711827f7a21aaeb90b63e0428ad8dba29d
sasza <sasza@onet.eu>
2019-09-21 00:06:13 +0200

    app main refactor

--- app/main.js
+++ app/main.js
@@ -5,25 +5,32 @@ import electron from 'electron'
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
 
-let mainWindow;
+let mainWindow
 
 function createWindow() {
-  mainWindow = new BrowserWindow({width: 900, height: 680, webPreferences: { webSecurity: false,  webviewTag: true }});
-  mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+  mainWindow = new BrowserWindow({
+    width: 900,
+    height: 680,
+    webPreferences: {
+      webSecurity: false, 
+      webviewTag: true
+    }
+  })
+  mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`)
   //mainWindow.webContents.openDevTools()
-  mainWindow.on('closed', () => mainWindow = null);
+  mainWindow.on('closed', () => mainWindow = null)
 }
 
-app.on('ready', createWindow);
+app.on('ready', createWindow)
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit();
+    app.quit()
   }
 })
 
 app.on('activate', () => {
   if (mainWindow === null) {
-    createWindow();
+    createWindow()
   }
 })
