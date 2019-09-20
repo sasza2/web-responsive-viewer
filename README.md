ae9ab6d7a001dd90801d9b9ed1525c4c9a4fa220
sasza <sasza@onet.eu>
2019-09-20 17:51:00 +0200

    parent window

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 npm install
 
 ## dev
-./node_modules/electron/dist/electron .
+npm run dev
 
 ## faq 
 on error `The SUID sandbox helper binary was found, but is not configured correctly` run `sudo sysctl kernel.unprivileged_userns_clone=1`
--- scripts/start.js
+++ scripts/start.js
@@ -36,34 +36,9 @@ const useYarn = fs.existsSync(paths.yarnLockFile);
 const isInteractive = process.stdout.isTTY;
 
 const electron = require('electron')
-const app = electron.app
-const BrowserWindow = electron.BrowserWindow
+const WindowParent = require('../src/windows/Parent')
+WindowParent(electron)
 
-const path = require('path')
-const isDev = require('electron-is-dev')
-
-let mainWindow;
-
-function createWindow() {
-  mainWindow = new BrowserWindow({width: 900, height: 680, webPreferences: { webSecurity: false,  webviewTag: true }});
-  mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
-  //mainWindow.webContents.openDevTools()
-  mainWindow.on('closed', () => mainWindow = null);
-}
-
-app.on('ready', createWindow);
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-})
-
-app.on('activate', () => {
-  if (mainWindow === null) {
-    createWindow();
-  }
-})
 
 // Warn and crash if required files are missing
 if (!checkRequiredFiles([paths.appHtml, paths.appIndexJs])) {
