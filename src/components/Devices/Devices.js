9d8ae0239e987a4a4c7bc913d3908456b6731baf
sasza <sasza@onet.eu>
2019-09-22 19:55:25 +0200

    add panzoom

--- src/components/Devices/Devices.js
+++ src/components/Devices/Devices.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import PanZoom from 'react-easy-panzoom'
 
 import WebView from '../WebView'
 
 import './Devices.sass'
 
 const Devices = () => (
-  <div className='devices'>
-    <WebView width={320} height={480} />
-    <WebView width={360} height={740} />
-    <WebView width={375} height={667} />
-    <WebView width={480} height={853} />
-    <WebView width={768} height={1024} />
-  </div>
+  <PanZoom>
+    <div className='devices'>
+      <WebView width={320} height={480} />
+      <WebView width={360} height={740} />
+      <WebView width={375} height={667} />
+      <WebView width={480} height={853} />
+      <WebView width={768} height={1024} />
+    </div>
+  </PanZoom>
 )
 
 export default Devices
