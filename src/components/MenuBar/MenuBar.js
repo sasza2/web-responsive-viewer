51d935eb1da3dc614838a79f5acc6dcd8bebcf3f
sasza <sasza@onet.eu>
2019-09-22 19:21:09 +0200

    Search component

--- src/components/MenuBar/MenuBar.js
+++ src/components/MenuBar/MenuBar.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
 import Tabs from './Tabs'
+import Search from './Search'
 
 import './MenuBar.sass'
 
 const MenuBar = () => (
   <div className='menu-bar'>
     <Tabs />
+    <Search />
   </div>
 )
 
---
+++ src/components/MenuBar/Search/Search.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './Search.sass'
+
+const Search = () => (
+  <div className='search'>
+
+  </div>
+)
+
+export default Search
