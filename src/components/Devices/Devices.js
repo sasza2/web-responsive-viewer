a4d0673204627891ee1c4197e9354a38314f06ab
sasza <sasza@onet.eu>
2019-09-22 20:01:42 +0200

    add masonry

--- src/components/Devices/Devices.js
+++ src/components/Devices/Devices.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PanZoom from 'react-easy-panzoom'
+import Masonry from 'react-masonry-component'
 
 import WebView from '../WebView'
 
@@ -8,11 +9,13 @@ import './Devices.sass'
 const Devices = () => (
   <PanZoom>
     <div className='devices'>
-      <WebView width={320} height={480} />
-      <WebView width={360} height={740} />
-      <WebView width={375} height={667} />
-      <WebView width={480} height={853} />
-      <WebView width={768} height={1024} />
+      <Masonry>
+        <WebView width={320} height={480} />
+        <WebView width={360} height={740} />
+        <WebView width={375} height={667} />
+        <WebView width={480} height={853} />
+        <WebView width={768} height={1024} />
+      </Masonry>
     </div>
   </PanZoom>
 )
