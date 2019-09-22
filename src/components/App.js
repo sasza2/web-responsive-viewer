849c4254355ac5f9abeedd34811daee2504c3ec7
sasza <sasza@onet.eu>
2019-09-22 17:38:50 +0200

    style background

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,11 @@
-import React from 'react';
+import React from 'react'
 
-import WebView from './WebView'
+import Devices from './Devices'
+
+import './App.sass'
 
 const App = () => (
-  <div style={{ display: 'flex', 'align-items': 'flex-start', transform: 'scale(0.5)', 'transformOrigin': 'top left' }}>
-    <WebView width={320} height={480} />
-    <WebView width={360} height={740} />
-    <WebView width={375} height={667} />
-    <WebView width={480} height={853} />
-    <WebView width={768} height={1024} />
-  </div>
+  <Devices />
 )
 
 export default App
