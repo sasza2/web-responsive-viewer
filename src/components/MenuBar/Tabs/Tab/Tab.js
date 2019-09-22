a3691aa066bd44f0866cfc046e2f25076fc05e26
sasza <sasza@onet.eu>
2019-09-22 19:49:26 +0200

    tab styles

--- src/components/MenuBar/Tabs/Tab/Tab.js
+++ src/components/MenuBar/Tabs/Tab/Tab.js
@@ -5,7 +5,8 @@ import './Tab.sass'
 const Tab = ({ active }) => (
   <div className={`tab ${active ? 'active' : ''}`}>
     <div className='tab__in'>
-      elo siemanderko xd
+      <span className='tab__name'>Lorem ipsum dolor sit amet, consectetur</span>
+      <span className='close'>x</span>
     </div>
   </div>
 )
