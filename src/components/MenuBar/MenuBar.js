bbd46697dd4678cd3544f4eb4e40a2d0b9ef60e5
sasza <sasza@onet.eu>
2019-09-22 19:18:43 +0200

    Tabs component

--- src/components/MenuBar/MenuBar.js
+++ src/components/MenuBar/MenuBar.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
+import Tabs from './Tabs'
+
 import './MenuBar.sass'
 
 const MenuBar = () => (
   <div className='menu-bar'>
-
+    <Tabs />
   </div>
 )
 
---
+++ src/components/MenuBar/Tabs/Tabs.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './Tabs.sass'
+
+const Tabs = () => (
+  <div className='tabs'>
+
+  </div>
+)
+
+export default Tabs
