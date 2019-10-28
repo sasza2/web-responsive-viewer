edba031ff7272cfe317776f1fbf4a64ba4d12795
sasza <sasza@onet.eu>
2019-10-28 20:33:45 +0100

    helpers file relative path

--- src/helpers/File.js
+++ src/helpers/File.js
@@ -1,11 +1,11 @@
 const fs = require('fs')
 
-const read = (path) => {
-  return fs.readFileSync(path, 'utf8')
-}
+const absolutePath = (relativePath) => `${process.env.REACT_APP_PATH}/store/${relativePath}`
 
-const write = (path, content) => {
-  fs.writeFileSync(path, content)
+const read = (relativePath) => fs.readFileSync(absolutePath(relativePath), 'utf8')
+
+const write = (relativePath, content) => {
+  fs.writeFileSync(absolutePath(relativePath), content)
 }
 
 const File = {
